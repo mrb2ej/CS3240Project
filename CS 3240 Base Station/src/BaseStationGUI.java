b9d57113c91da6ca977414f032e6688a9e109f62
Matthew Bongiovi <mrb2ej@virginia.edu>
2013-03-24 15:53:53 -0400

    Added basic GUI. No functionality so far

--- CS 3240 Base Station/src/BaseStationGUI.java
+++ CS 3240 Base Station/src/BaseStationGUI.java
@@ -1,12 +1,59 @@
+import java.awt.BorderLayout;
+import javax.swing.JLabel;
 
-public class BaseStationGUI {
+import javax.swing.WindowConstants;
+import javax.swing.SwingUtilities;
+
+
+/**
+* This code was edited or generated using CloudGarden's Jigloo
+* SWT/Swing GUI Builder, which is free for non-commercial
+* use. If Jigloo is being used commercially (ie, by a corporation,
+* company or business for any purpose whatever) then you
+* should purchase a license for each developer using Jigloo.
+* Please visit www.cloudgarden.com for details.
+* Use of Jigloo implies acceptance of these licensing terms.
+* A COMMERCIAL LICENSE HAS NOT BEEN PURCHASED FOR
+* THIS MACHINE, SO JIGLOO OR THIS CODE CANNOT BE USED
+* LEGALLY FOR ANY CORPORATE OR COMMERCIAL PURPOSE.
+*/
+public class BaseStationGUI extends javax.swing.JFrame {
+	private JLabel lightSensorLabel;
 
 	/**
-	 * @param args
-	 */
+	* Auto-generated main method to display this JFrame
+	*/
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				BaseStationGUI inst = new BaseStationGUI();
+				inst.setLocationRelativeTo(null);
+				inst.setVisible(true);
+			}
+		});
+	}
+	
+	public BaseStationGUI() {
+		super();
+		initGUI();
+	}
+	
+	private void initGUI() {
+		try {
+			setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+			getContentPane().setLayout(null);
+			{
+				lightSensorLabel = new JLabel();
+				getContentPane().add(lightSensorLabel);
+				lightSensorLabel.setText("Light:");
+				lightSensorLabel.setBounds(28, 33, 56, 16);
+			}
+			pack();
+			this.setSize(413, 320);
+		} catch (Exception e) {
+		    //add your error handling code here
+			e.printStackTrace();
+		}
 	}
 
 }
